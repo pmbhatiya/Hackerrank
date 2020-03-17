9d34bf6af4987f6edb00cd9b67e351f8d51d5b13
pm bhatiya <61078575+pmbhatiya@users.noreply.github.com>
2020-03-17 17:21:58 +0530

    Add files via upload

---
+++ Lists comprehensions.py
@@ -0,0 +1,6 @@
+#list comprehensions on python Domain HackerRank Solutions   
+#!/usr/bin/python3
+
+x, y, z, n = (int(input()) for i in range(4))
+
+print ([[a, b, c] for a in range(0, x + 1) for b in range(0, y + 1) for c in range(0, z + 1) if a + b + c != n]);
