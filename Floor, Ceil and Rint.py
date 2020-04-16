956abc8d0635f4b19f0287035b50f113e654555c
pm bhatiya <61078575+pmbhatiya@users.noreply.github.com>
2020-04-16 09:31:05 +0530

    Add files via upload

---
+++ Floor, Ceil and Rint.py
@@ -0,0 +1,9 @@
+# Python Floor, Ceil and Rint HackerRank solution
+
+import numpy as np
+np.set_printoptions(sign=' ')
+
+array=np.array(input().split(),float)
+print(np.floor(array))
+print(np.ceil(array))
+print(np.rint(array))
